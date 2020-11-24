0fbd18817bf80279602c29831d3ba11f790af865
Ritik Srivastava <ritik.space@gmail.com>
2020-11-24 17:19:39 +0530

    Add Region Selector

--- script.ps1
+++ script.ps1
@@ -3,13 +3,15 @@ Install-Module powershell-yaml
 [System.Reflection.Assembly]::LoadWithPartialName('presentationframework') 	 | out-null
 [System.Reflection.Assembly]::LoadWithPartialName('System.Drawing') 		 | out-null
 [System.Reflection.Assembly]::LoadWithPartialName('WindowsFormsIntegration') | out-null
-$Current_Folder = split-path $MyInvocation.MyCommand.Path;
+
+$Current_Folder = split-path $MyInvocation.MyCommand.Path
 $Binaries = "$Current_Folder\bin"
+$config_url = ".\bin\config.yml"
 
 ################################################################################################################################
 # Parse config.yml
 ################################################################################################################################
-$fileContent = Get-Content -Path ".\bin\config.yml"
+$fileContent = Get-Content -Path $config_url
 $content = ''
 foreach ($line in $fileContent) { 
 	$content = $content + "`n" + $line 
@@ -20,13 +22,19 @@ $config = ConvertFrom-YAML $content
 ################################################################################################################################
 # Start NGROK Server
 ################################################################################################################################
-
-$ngrok = "$Binaries\ngrok.exe"
 $arguments = "http -config $Binaries\auth.yml -config $Binaries\config.yml 3000"
-$process = Start-Process $ngrok $arguments -WindowStyle Hidden -passthru
+$scriptblock = [scriptblock]::Create("$Binaries\ngrok.exe $arguments")
+start-job -scriptblock $scriptblock
 
-$ngrokOutput = ConvertFrom-Json (Invoke-WebRequest -Uri http://localhost:4040/api/tunnels).Content
-$public_urls = $ngrokOutput.tunnels.public_url | sort;
+$ngrokOutput = $NULL
+do  
+{
+	Start-Sleep -s 1
+	$ngrokOutput = ConvertFrom-Json (Invoke-WebRequest -Uri http://localhost:4040/api/tunnels).Content
+}
+while($ngrokOutput.tunnels.length -lt 2)  
+
+$public_urls = $ngrokOutput.tunnels.public_url | sort
 $httpUrl = $public_urls[0]
 $httpsUrl = $public_urls[1]
 
@@ -38,15 +46,14 @@ $httpsUrl = $public_urls[1]
 # Caching all Icons
 # -----------------------------------------------------------------------------------
 $App_Tray_Icon = "$Current_Folder\icons\logo.ico"
-$Dashboard_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\dashboard.png");
-$Web_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\web.png");
-$Copy_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\copy.png");
-$Safe_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\safe.png");
-$Unsafe_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\unsafe.png");
-$Region_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\region.png");
-$Tick_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\tick.png");
-$Cross_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\exit.png");
-
+$Dashboard_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\dashboard.png")
+$Web_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\web.png")
+$Copy_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\copy.png")
+$Safe_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\safe.png")
+$Unsafe_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\unsafe.png")
+$Region_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\region.png")
+$Tick_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\tick.png")
+$Cross_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\exit.png")
 
 $App_Icon = New-Object System.Windows.Forms.NotifyIcon
 $App_Icon.Text = "NGROK Runner"
@@ -64,7 +71,7 @@ $App_Icon.ContextMenuStrip = $contextmenu
 # -----------------------------------------------------------------------------------
 # Adding Submenu `Open Dashboard`
 # -----------------------------------------------------------------------------------
-$Dashboard = $contextmenu.Items.Add("Open Dashboard");
+$Dashboard = $contextmenu.Items.Add("Open Dashboard")
 $Dashboard.Image = $Dashboard_Icon
 $Dashboard.Add_Click({
 	Start-Process 'http://localhost:4040'
@@ -73,7 +80,7 @@ $Dashboard.Add_Click({
 # -----------------------------------------------------------------------------------
 # Adding Submenu `Open`
 # -----------------------------------------------------------------------------------
-$Open_SubMenu = $contextmenu.Items.Add("Open in web browser");
+$Open_SubMenu = $contextmenu.Items.Add("Open in web browser")
 $Open_SubMenu.Image = $Web_Icon
 
 $OpenHttps = New-Object System.Windows.Forms.ToolStripMenuItem
@@ -95,7 +102,7 @@ $Open_SubMenu.DropDownItems.Add($OpenHttp)
 # -----------------------------------------------------------------------------------
 # Adding Submenu `Copy to clipboard`
 # -----------------------------------------------------------------------------------
-$Copy_SubMenu = $contextmenu.Items.Add("Copy to clipboard");
+$Copy_SubMenu = $contextmenu.Items.Add("Copy to clipboard")
 $Copy_SubMenu.Image = $Copy_Icon
 
 $CopyHttps = New-Object System.Windows.Forms.ToolStripMenuItem
@@ -118,15 +125,17 @@ $Copy_SubMenu.DropDownItems.Add($CopyHttp)
 # Adding Submenu `Region (Country)`
 # -----------------------------------------------------------------------------------
 $Selected_Region = 'us'
+
 if ($config.region -ne $NULL) {
 	$Selected_Region = $config.region
 }
 
-$Region_SubMenu = $contextmenu.Items.Add("Region ($Selected_Region)");
+$Region_SubMenu = $contextmenu.Items.Add("Region ($Selected_Region)")
 $Region_SubMenu.Image = $Region_Icon
 
 $Country_Names = @("Asia/Pacific","Australia","Europe","India","Japan","South America","United States")
 $Country_Codes = @("ap","au","eu","in","jp","sa","us")
+
 for ($i = 0; $i -lt $Country_Names.Length; $i++) {
 	$Country = New-Object System.Windows.Forms.ToolStripMenuItem
 	$Country.Text = $Country_Names[$i]
@@ -134,20 +143,27 @@ for ($i = 0; $i -lt $Country_Names.Length; $i++) {
 		$Country.Image = $Tick_Icon
 	}
 	$Country.Add_Click({
+		# Write new config.yml file
 		$config.region = $Country_Codes[$i]
-	})
+		$config_new = ConvertTo-Yaml $config
+		$config_new.trim() | Out-File -FilePath $config_url
+
+		# Restart NGROK Server
+		Start-Process -WindowStyle hidden powershell.exe "$Current_Folder\script.ps1"
+		$App_Icon.Visible = $false
+		Stop-Process -Id $pid
+	}.GetNewClosure())
 	$Region_SubMenu.DropDownItems.Add($Country)
 }
 
 # -----------------------------------------------------------------------------------
 # Adding Submenu `Quit`
 # -----------------------------------------------------------------------------------
-$Quit = $contextmenu.Items.Add("Quit");
+$Quit = $contextmenu.Items.Add("Quit")
 $Quit.Image = $Cross_Icon
 $Quit.Add_Click({
-	Stop-Process -Id $process.Id
 	$App_Icon.Visible = $false
-	Stop-Process $pid
+	Stop-Process -Id $pid
 })
 
 
