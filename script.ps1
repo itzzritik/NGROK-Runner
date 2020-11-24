2b49b7d4b5bb6dec62f82b13e176defdaa5792e9
Ritik Srivastava <ritik.space@gmail.com>
2020-11-24 05:54:13 +0530

    Add Icons to menu items

--- script.ps1
+++ script.ps1
@@ -14,91 +14,99 @@ $arguments = "http 3000"
 $process = Start-Process $ngrok $arguments -WindowStyle Hidden -passthru
 
 $ngrokOutput = ConvertFrom-Json (Invoke-WebRequest -Uri http://localhost:4040/api/tunnels).Content
-$httpUrl = $ngrokOutput.tunnels.public_url[0]
-$httpsUrl = $ngrokOutput.tunnels.public_url[1]
+$public_urls = $ngrokOutput.tunnels.public_url | sort;
+$httpUrl = $public_urls[0]
+$httpsUrl = $public_urls[1]
 
 ################################################################################################################################
 # Add the systray menu
 ################################################################################################################################
-	
+
+# -----------------------------------------------------------------------------------
+# Caching all Icons
+# -----------------------------------------------------------------------------------
+$App_Tray_Icon = "$Current_Folder\icons\logo.ico"
+$Dashboard_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\dashboard.png");
+$Web_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\web.png");
+$Copy_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\copy.png");
+$Safe_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\safe.png");
+$Unsafe_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\unsafe.png");
+$Exit_Icon = [System.Drawing.Bitmap]::FromFile("$Current_Folder\icons\exit.png");
+
+
 $App_Icon = New-Object System.Windows.Forms.NotifyIcon
 $App_Icon.Text = "NGROK Runner"
-$App_Icon.Icon = "$Current_Folder\icons\logo.ico"
+$App_Icon.Icon = $App_Tray_Icon
 $App_Icon.Visible = $true
+$App_Icon.Add_Click({
+	If ($_.Button -eq [Windows.Forms.MouseButtons]::Left) {
+		Start-Process $httpsUrl
+	}
+})
 
 $contextmenu = New-Object System.Windows.Forms.ContextMenuStrip
 $App_Icon.ContextMenuStrip = $contextmenu
 
+# Adding item `Open Dashboard`
+$Dashboard = $contextmenu.Items.Add("Open NGROK Dashboard");
+$Dashboard.Image = $Dashboard_Icon
+$Dashboard.Add_Click({
+	Start-Process 'http://localhost:4040'
+})
+
+# -----------------------------------------------------------------------------------
 # Adding Submenu `Open`
+# -----------------------------------------------------------------------------------
 $Open_SubMenu = $contextmenu.Items.Add("Open In Web Browser");
+$Open_SubMenu.Image = $Web_Icon
 
 $OpenHttp = New-Object System.Windows.Forms.ToolStripMenuItem
 $OpenHttp.Text = "HTTP App"
+$OpenHttp.Image = $Unsafe_Icon
+$OpenHttp.Add_Click({
+	Start-Process $httpUrl
+})
 $Open_SubMenu.DropDownItems.Add($OpenHttp)
 
 $OpenHttps = New-Object System.Windows.Forms.ToolStripMenuItem
 $OpenHttps.Text = "HTTPS App"
+$OpenHttps.Image = $Safe_Icon
+$OpenHttps.Add_Click({
+	Start-Process $httpsUrl
+})
 $Open_SubMenu.DropDownItems.Add($OpenHttps)
 
+# -----------------------------------------------------------------------------------
 # Adding Submenu `Copy to clipboard`
+# -----------------------------------------------------------------------------------
 $Copy_SubMenu = $contextmenu.Items.Add("Copy To Clipboard");
+$Copy_SubMenu.Image = $Copy_Icon
 
 $CopyHttp = New-Object System.Windows.Forms.ToolStripMenuItem
 $CopyHttp.Text = "HTTP Url"
+$CopyHttp.Image = $Unsafe_Icon
+$CopyHttp.Add_Click({
+	$httpUrl | clip
+})
 $Copy_SubMenu.DropDownItems.Add($CopyHttp)
 
 $CopyHttps = New-Object System.Windows.Forms.ToolStripMenuItem
 $CopyHttps.Text = "HTTPS Url"
+$CopyHttps.Image = $Safe_Icon
+$CopyHttps.Add_Click({
+	$httpsUrl | clip
+})
 $Copy_SubMenu.DropDownItems.Add($CopyHttps)
 
 # Adding item `Copy to clipboard`
 $Exit = $contextmenu.Items.Add("Exit");
-
-# ---------------------------------------------------------------------
-# Action when after a click on the systray icon
-# ---------------------------------------------------------------------
-$App_Icon.Add_Click({
-	If ($_.Button -eq [Windows.Forms.MouseButtons]::Left) {
-		Start-Process 'http://localhost:4040'
-	}
-})
-
-# ---------------------------------------------------------------------
-# Action after clicking on Open Http URL
-# ---------------------------------------------------------------------
-$OpenHttp.Add_Click({
-	Start-Process $httpUrl
-})
-
-# ---------------------------------------------------------------------
-# Action after clicking on Open Https URL
-# ---------------------------------------------------------------------
-$OpenHttps.Add_Click({
-	Start-Process $httpsUrl
-})
-
-# ---------------------------------------------------------------------
-# Action after clicking on Open Https URL
-# ---------------------------------------------------------------------
-$CopyHttp.Add_Click({
-	$httpUrl | clip
-})
-
-# ---------------------------------------------------------------------
-# Action after clicking on Open Https URL
-# ---------------------------------------------------------------------
-$CopyHttps.Add_Click({
-	$httpsUrl | clip
-})
-
-# ---------------------------------------------------------------------
-# When Exit is clicked, shutdown NGROK and kill the PowerShell process
-# ---------------------------------------------------------------------
+$Exit.Image = $Exit_Icon
 $Exit.Add_Click({
 	Stop-Process -Id $process.Id
 	$App_Icon.Visible = $false
 	Stop-Process $pid
 })
+
 
 # Make PowerShell Disappear
 $windowcode = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
